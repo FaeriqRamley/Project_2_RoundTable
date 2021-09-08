213f39225d6ba2f03483df97baf31d8fc81effc6
faeriqramley <mohamad.faeriq.ramley@gmail.com>
2021-09-08 21:42:51 +0800

    added subfolders to reducers

--- react-file/src/reducers/index.js
+++ react-file/src/reducers/index.js
@@ -1,11 +1,11 @@
-import currentUserReducer from "./currentUserReducer";
-import searchParamsReducer from "./searchParamsReducer";
-import newsDataReducer from "./newsDataReducer";
-import apiKeysReducer from "./apiKeysReducer";
-import selectedPublisherReducer from "./selectedPublisherReducer";
-import activeArticleReducer from "./activeArticleReducer";
-import commentListReducer from "./commentListReducer";
-import currentUserStorageReducer from './currentUserStorageReducer';
+import currentUserReducer from "./firebase-reducers/currentUserReducer";
+import searchParamsReducer from "./appfunction-reducers/searchParamsReducer";
+import newsDataReducer from "./api-reducers/newsDataReducer";
+import apiKeysReducer from "./api-reducers/apiKeysReducer";
+import selectedPublisherReducer from "./appfunction-reducers/selectedPublisherReducer";
+import activeArticleReducer from "./appfunction-reducers/activeArticleReducer";
+import commentListReducer from "./firebase-reducers/commentListReducer";
+import currentUserStorageReducer from './firebase-reducers/currentUserStorageReducer';
 import { combineReducers } from "redux";
 
 const rootReducer = combineReducers({
